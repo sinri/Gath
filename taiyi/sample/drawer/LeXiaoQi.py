1f26c71929b59e0061d5ebfa3cdcd1e29f03a2aa
Sinri Edogawa <e.joshua.s.e@gmail.com>
2023-06-01 11:36:08 +0800

    textual_inversion

--- taiyi/sample/drawer/LeXiaoQi.py
+++ taiyi/sample/drawer/LeXiaoQi.py
@@ -1,42 +1,18 @@
 import time
 
-import torch
-from safetensors.torch import safe_open
+import yaml
+from tokenizers import Tokenizer
+from transformers import CLIPTokenizer
 
 from taiyi.drawer.TaiyiDrawer import TaiyiDrawer
+from taiyi.drawer.TaiyiMetaDrawer import TaiyiMetaDrawer
 from taiyi.sample.drawer.SampleConstant import SampleConstant
 
 if __name__ == '__main__':
-    safetensors_file="E:\\sinri\\sd-scripts\\workspace\\lxq\\trained_model\\openjourney-lxq-1.safetensors"
+    with open('meta/LeXiaoQi-0001.yml', 'r', encoding='utf-8') as f:
+        draw_meta = yaml.safe_load(f)
+    print(draw_meta)
 
-    # tensors = {}
-    # with safe_open(safetensors_file, framework="pt", device=0) as f:
-    #     for k in f.keys():
-    #         tensors[k] = f.get_tensor(k)
-    #         print(tensors[k])
-
-    model=torch.load(safetensors_file)
-    model.eval()
-
-    # # 提示词
-    # prompt = "lxq robot"
-    # # 降噪步数 数字越大，时间越长
-    # num_inference_steps = 75
-    # # 遵循提示词的级别 数字越大越接近提示词，但图像质量会下降
-    # guidance_scale = 10
-    # # 负面提示词
-    # # negative_prompt = "雨天"
-    #
-    # drawn = d.draw(
-    #     prompt,
-    #     height=512,
-    #     width=512,
-    #     num_inference_steps=num_inference_steps,
-    #     guidance_scale=guidance_scale,
-    #     # negative_prompt=negative_prompt
-    # )
-    # image = drawn.images[0]
-    # # image.show()
-    # filename = SampleConstant.output_dir + f"\\LeXiaoQi-{time.time()}.jpg"
-    # image.save(filename)
-    # print(f"saved: {filename}")
+    filename = SampleConstant.output_dir + f"\\LeXiaoQi-{time.time()}.jpg"
+    TaiyiMetaDrawer(draw_meta).draw(filename)
+    print(f"saved: {filename}")
--- taiyi/sample/drawer/SampleConstant.py
+++ taiyi/sample/drawer/SampleConstant.py
@@ -3,4 +3,6 @@ class SampleConstant:
     taiyi_model = "E:\\sinri\\Taiyi-Stable-Diffusion-1B-Chinese-v0.1"
     waifu_model = "E:\\sinri\\stable-diffusion-webui\\models\\Stable-diffusion\\waifu-diffusion"
 
+    textual_inversion_lxq_alpha='E:\\sinri\\stable-diffusion-webui\\embeddings\\a blue-white monster named lxq.pt'
+
     output_dir = "E:\\sinri\\TaiyiDrawer\\output"
